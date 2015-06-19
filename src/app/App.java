4f96bcc721aa1bc71e4096d737cb2f874e897de1
Raffaele Sgarro <raffaelesgarro@gmail.com>
2015-06-19 23:04:30 +0200

    Extracted SendMailStage

--- src/app/App.java
+++ src/app/App.java
@@ -1,55 +1,34 @@
 package app;
 
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.HBoxBuilder;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
-import javax.mail.MessagingException;
 import java.io.*;
-import java.util.List;
 import java.util.Properties;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class App extends Application {
 
-    private static final Logger log = Logger.getLogger("app.App");
+    private static final Logger log = Logger.getLogger(App.class.getName());
 
     public static final String HOME_DIR = System.getProperty("user.home") + System.getProperty("file.separator") + ".lara-chipmunk";
     public static final String CONF_FILE_NAME = "conf.properties";
 
     private final Properties conf = new Properties();
 
-    private final TextField host = new TextField();
-    private final TextField port = new TextField();
-    private final TextField user = new TextField();
-    private final TextField password = new PasswordField();
-    private final CheckBox auth = new CheckBox("Auth");
-    private final CheckBox startTls = new CheckBox("Start TLS");
-    private final Button testConnectionBtn = new Button("Test");
-    private final Button saveSettingsBtn = new Button("Salva");
-
     private final Button setAttachmentsDirBtn = new Button("Scegli la directory che contiene gli allegati");
     private final ListView<File> files = new ListView<>();
-    private final Label selectedFile = new Label("Nessun file selezionato");
-
-    private final TextField from = new TextField();
-    private final TextField to = new TextField();
-    private final TextField subject = new TextField();
-    private final Button sendBtn = new Button("Invia");
-    private final TextArea message = new TextArea();
 
     private Stage mainWindow;
     private boolean mockMailService;
@@ -65,25 +44,41 @@ public class App extends Application {
 
         mainWindow = stage;
         VBox root = layout();
-        setValuesFrom(conf);
         setUpEventsHandling();
 
         Scene scene = new Scene(root);
         stage.setTitle("Invia i file per email");
         stage.setScene(scene);
-        stage.setWidth(700);
-        stage.setHeight(600);
+        stage.setWidth(750);
+        stage.setHeight(500);
         stage.show();
     }
 
-    private void setValuesFrom(Properties conf) {
-        host.setText(conf.getProperty("mail.smtp.host"));
-        port.setText(conf.getProperty("mail.smtp.port"));
-        user.setText(conf.getProperty("user"));
-        // password is not stored for security reasons
-        auth.selectedProperty().setValue(Boolean.parseBoolean(conf.getProperty("mail.smtp.auth")));
-        startTls.selectedProperty().setValue(Boolean.parseBoolean(conf.getProperty("mail.smtp.starttls.enable")));
-        from.setText(conf.getProperty("from"));
+    private void setUpEventsHandling() {
+
+        setAttachmentsDirBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                DirectoryChooser chooser = new DirectoryChooser();
+                chooser.setTitle("Scegli la directory");
+                File dir = chooser.showDialog(mainWindow);
+                if (dir != null && dir.exists()) {
+                    showDirContents(dir);
+                }
+            }
+        });
+
+        files.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if (event.getClickCount() == 2) {
+                    File attachmentFile = files.selectionModelProperty().get().getSelectedItem();
+                    SendMailStage stage = new SendMailStage(App.this, attachmentFile);
+                    stage.onBeforeShow();
+                    stage.show();
+                }
+            }
+        });
     }
 
     private void ensureConfFileExists() throws IOException {
@@ -106,144 +101,11 @@ public class App extends Application {
         }
     }
 
-    private void storeProperties(Properties p) throws IOException {
+    public void storeProperties(Properties p) throws IOException {
         // Use default platform encoding
         FileWriter out = new FileWriter(new File(HOME_DIR, CONF_FILE_NAME));
         p.store(out, "Edit this file to change app settings");
         out.close();
-    }
-
-    private void setUpEventsHandling() {
-
-        setAttachmentsDirBtn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                DirectoryChooser chooser = new DirectoryChooser();
-                chooser.setTitle("Scegli la directory");
-                File dir = chooser.showDialog(mainWindow);
-                if (dir != null && dir.exists()) {
-                    showDirContents(dir);
-                }
-            }
-        });
-
-        sendBtn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                sendMailSpec(getSnapshot());
-            }
-        });
-
-        files.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<File>() {
-            @Override
-            public void changed(ObservableValue<? extends File> observableValue, File old, File current) {
-                selectedFile.setText(current != null ? current.getAbsolutePath() : "Nessun file selezionato");
-            }
-        });
-
-        saveSettingsBtn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                conf.setProperty("user", user.getText());
-                conf.setProperty("from", from.getText());
-                // Password is not stored
-                conf.setProperty("mail.smtp.host", host.getText());
-                conf.setProperty("mail.smtp.port", port.getText());
-                conf.setProperty("mail.smtp.auth", Boolean.toString(auth.selectedProperty().get()));
-                conf.setProperty("mail.smtp.starttls.enable", Boolean.toString(startTls.selectedProperty().get()));
-                try{
-                    storeProperties(conf);
-                } catch (IOException e) {
-                    log.log(Level.SEVERE, "Could not save config file", e);
-                }
-            }
-        });
-
-        testConnectionBtn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                MailSpec spec = getSnapshot();
-                spec.to = spec.from;
-                spec.subject = "Test from Lara Chipmunk";
-                spec.message = "It works!";
-                sendMailSpec(spec);
-            }
-        });
-    }
-
-    private void sendMailSpec(MailSpec spec) {
-        try {
-            validate(spec);
-            MessagingConsole messagingConsole = new MessagingConsole();
-            messagingConsole.clear();
-            messagingConsole.show();
-            send(spec, messagingConsole);
-        } catch (ValidationException e) {
-            showInvalidDataDialog(e);
-        } catch (MessagingException e) {
-            log.severe(e.getMessage());
-            log.log(Level.SEVERE, "Could not send message", e);
-            showErrorDialog(e.getMessage());
-        }
-    }
-
-    private void showErrorDialog(String message) {
-        Dialog dialog = new Dialog();
-        dialog.setTitle("Errore");
-        dialog.setText(message);
-        dialog.show();
-    }
-
-    private void showInvalidDataDialog(ValidationException e) {
-        DialogWithLengthyText dialog = new DialogWithLengthyText();
-        dialog.setTitle("Errore");
-        dialog.setShortText(e.getMessage());
-        StringBuilder longText = new StringBuilder();
-        for (String msg : e.errors) longText.append(msg).append("\n");
-        dialog.setLongText(longText.toString());
-        dialog.show();
-    }
-
-    private void validate(MailSpec spec) throws ValidationException {
-        // TODO implement Just let it fail for now
-    }
-
-    private void chk(String str, String desc, List<String> errors) {
-        String msg = desc + " non può essere vuoto";
-        if (str == null || str.isEmpty()) {
-            errors.add(msg);
-        }
-    }
-
-    private MailSpec getSnapshot() {
-        MailSpec s = new MailSpec();
-        s.host = host.getText();
-        s.port = port.getText();
-        s.user = user.getText();
-        s.password = password.getText();
-        s.from = from.getText();
-        s.to = to.getText();
-        s.subject = subject.getText();
-        s.message = message.getText();
-        s.file = files.getSelectionModel().getSelectedItem();
-        s.startTls = Boolean.toString(startTls.selectedProperty().get());
-        s.smtpAuth = Boolean.toString(auth.selectedProperty().get());
-        return s;
-    }
-
-    private void send(MailSpec src, MessagingConsole console) throws MessagingException {
-        Runnable r;
-
-        if (mockMailService) {
-            r = new MockSendMailRunnable(src, console);
-        } else {
-            r = new SendMailRunnable(src, console);
-        }
-
-        Thread t = new Thread(r);
-        t.setName("sendmail");
-        t.setDaemon(true);
-        t.start();
     }
 
     private void showDirContents(File dir) {
@@ -260,46 +122,22 @@ public class App extends Application {
 
         root.setSpacing(10);
 
-        HBox settings = new HBox();
-        settings.setSpacing(10);
-        settings.setMaxWidth(Double.MAX_VALUE);
-        settings.getChildren().setAll(to, sendBtn);
-        HBox.setHgrow(to, Priority.ALWAYS);
-
-        host.setPromptText("Server");
-        port.setPromptText("Porta");
-        user.setPromptText("Utente");
-        password.setPromptText("Password");
-        port.setPrefWidth(50);
-
-        from.setPromptText("Da");
-        to.setPromptText("Destinatario");
-        subject.setPromptText("Oggetto della mail");
-        message.setPromptText("Corpo del testo");
-
         root.getChildren().setAll(
-                  from
-                , HBoxBuilder.create().children(host, port, user, password, auth, startTls, testConnectionBtn, saveSettingsBtn)
-                        .prefWidth(Double.MAX_VALUE)
-                        .spacing(10)
-                        .build()
-                , setAttachmentsDirBtn
+                setAttachmentsDirBtn
                 , files
-                , selectedFile
-                , subject
-                , message
-                , settings
         );
 
-        HBox.setHgrow(host, Priority.ALWAYS);
-        host.setPrefWidth(100);
-        password.setPrefWidth(100);
-
         files.setPrefHeight(100);
-        message.setPrefHeight(60);
         VBox.setVgrow(files, Priority.SOMETIMES);
-        VBox.setVgrow(message, Priority.SOMETIMES);
 
         return root;
+    }
+
+    public boolean isMockMailService() {
+        return mockMailService;
+    }
+
+    public Properties getConfiguration() {
+        return conf;
     }
 }
---
+++ src/app/SendMailStage.java
@@ -0,0 +1,222 @@
+package app;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.HBoxBuilder;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import javax.mail.MessagingException;
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class SendMailStage extends Stage {
+
+    private static final Logger log = Logger.getLogger(SendMailStage.class.getName());
+
+    private final App app;
+    private final File attachmentFile;
+
+    private final TextField host = new TextField();
+    private final TextField port = new TextField();
+    private final TextField user = new TextField();
+    private final TextField password = new PasswordField();
+    private final CheckBox auth = new CheckBox("Auth");
+    private final CheckBox startTls = new CheckBox("Start TLS");
+    private final Button testConnectionBtn = new Button("Test");
+    private final Button saveSettingsBtn = new Button("Salva");
+
+    private final TextField from = new TextField();
+    private final TextField to = new TextField();
+    private final TextField subject = new TextField();
+    private final Button sendBtn = new Button("Invia");
+    private final TextArea message = new TextArea();
+
+    public SendMailStage(App app, File attachmentFile) {
+        this.app = app;
+        this.attachmentFile = attachmentFile;
+
+        setScene(new Scene(layout()));
+
+        setUpEventsHandling();
+
+        setWidth(700);
+        setHeight(450);
+
+        setTitle("Compose: " + attachmentFile.getName());
+    }
+
+    private void setUpEventsHandling() {
+
+        sendBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                sendMailSpec(getSnapshot());
+            }
+        });
+
+        saveSettingsBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Properties conf = app.getConfiguration();
+                conf.setProperty("user", user.getText());
+                conf.setProperty("from", from.getText());
+                // Password is not stored
+                conf.setProperty("mail.smtp.host", host.getText());
+                conf.setProperty("mail.smtp.port", port.getText());
+                conf.setProperty("mail.smtp.auth", Boolean.toString(auth.selectedProperty().get()));
+                conf.setProperty("mail.smtp.starttls.enable", Boolean.toString(startTls.selectedProperty().get()));
+                try {
+                    app.storeProperties(conf);
+                } catch (IOException e) {
+                    log.log(Level.SEVERE, "Could not save config file", e);
+                }
+            }
+        });
+
+        testConnectionBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                MailSpec spec = getSnapshot();
+                spec.to = spec.from;
+                spec.subject = "Test from Lara Chipmunk";
+                spec.message = "It works!";
+                sendMailSpec(spec);
+            }
+        });
+    }
+
+    public void onBeforeShow() {
+        setValuesFrom(app.getConfiguration());
+    }
+
+    private VBox layout() {
+        VBox root = new VBox();
+        root.setPadding(new Insets(10));
+
+        root.setSpacing(10);
+
+        HBox settings = new HBox();
+        settings.setSpacing(10);
+        settings.setMaxWidth(Double.MAX_VALUE);
+        settings.getChildren().setAll(to, sendBtn);
+        HBox.setHgrow(to, Priority.ALWAYS);
+
+        host.setPromptText("Server");
+        port.setPromptText("Porta");
+        user.setPromptText("Utente");
+        password.setPromptText("Password");
+        port.setPrefWidth(50);
+
+        from.setPromptText("Da");
+        to.setPromptText("Destinatario");
+        subject.setPromptText("Oggetto della mail");
+        message.setPromptText("Corpo del testo");
+
+        root.getChildren().setAll(
+                  from
+                , HBoxBuilder.create().children(host, port, user, password, auth, startTls, testConnectionBtn, saveSettingsBtn)
+                        .prefWidth(Double.MAX_VALUE)
+                        .spacing(10)
+                        .build()
+                , subject
+                , message
+                , settings
+        );
+
+        HBox.setHgrow(host, Priority.ALWAYS);
+        host.setPrefWidth(100);
+        password.setPrefWidth(100);
+
+        message.setPrefHeight(60);
+        VBox.setVgrow(message, Priority.SOMETIMES);
+
+        return root;
+    }
+
+    private void sendMailSpec(MailSpec spec) {
+        try {
+            validate(spec);
+            MessagingConsole messagingConsole = new MessagingConsole();
+            messagingConsole.clear();
+            messagingConsole.show();
+            send(spec, messagingConsole);
+        } catch (ValidationException e) {
+            showInvalidDataDialog(e);
+        } catch (MessagingException e) {
+            log.severe(e.getMessage());
+            log.log(Level.SEVERE, "Could not send message", e);
+            showErrorDialog(e.getMessage());
+        }
+    }
+
+    private void setValuesFrom(Properties conf) {
+        host.setText(conf.getProperty("mail.smtp.host"));
+        port.setText(conf.getProperty("mail.smtp.port"));
+        user.setText(conf.getProperty("user"));
+        // password is not stored for security reasons
+        auth.selectedProperty().setValue(Boolean.parseBoolean(conf.getProperty("mail.smtp.auth")));
+        startTls.selectedProperty().setValue(Boolean.parseBoolean(conf.getProperty("mail.smtp.starttls.enable")));
+        from.setText(conf.getProperty("from"));
+    }
+
+    private void showErrorDialog(String message) {
+        Dialog dialog = new Dialog();
+        dialog.setTitle("Errore");
+        dialog.setText(message);
+        dialog.show();
+    }
+
+    private void showInvalidDataDialog(ValidationException e) {
+        DialogWithLengthyText dialog = new DialogWithLengthyText();
+        dialog.setTitle("Errore");
+        dialog.setShortText(e.getMessage());
+        StringBuilder longText = new StringBuilder();
+        for (String msg : e.errors) longText.append(msg).append("\n");
+        dialog.setLongText(longText.toString());
+        dialog.show();
+    }
+
+    private void validate(MailSpec spec) throws ValidationException {
+        // TODO implement Just let it fail for now
+    }
+
+    private MailSpec getSnapshot() {
+        MailSpec s = new MailSpec();
+        s.file = attachmentFile;
+        s.host = host.getText();
+        s.port = port.getText();
+        s.user = user.getText();
+        s.password = password.getText();
+        s.from = from.getText();
+        s.to = to.getText();
+        s.subject = subject.getText();
+        s.message = message.getText();
+        s.startTls = Boolean.toString(startTls.selectedProperty().get());
+        s.smtpAuth = Boolean.toString(auth.selectedProperty().get());
+        return s;
+    }
+
+    private void send(MailSpec src, MessagingConsole console) throws MessagingException {
+        Runnable r;
+
+        if (app.isMockMailService()) {
+            r = new MockSendMailRunnable(src, console);
+        } else {
+            r = new SendMailRunnable(src, console);
+        }
+
+        Thread t = new Thread(r);
+        t.setName("sendmail");
+        t.setDaemon(true);
+        t.start();
+    }
+}
